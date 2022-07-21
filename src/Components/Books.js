95cdfc49a88d10b0ce5f20e2cb9355529ea22fbb
Henry <dokinruss@gmail.com>
2022-07-21 16:08:24 +0000

    fix linter error

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -4,7 +4,7 @@ import FormInput from './FormInput';
 
 class Book extends React.Component {
    
-        state = {
+        state = { // eslint-disable-line
             books: [
               {
                 id: 1,
