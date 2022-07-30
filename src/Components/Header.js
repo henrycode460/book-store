5e4bb142dfa23600be1b6f2b2a3529484ab83c9f
Henry <dokinruss@gmail.com>
2022-07-30 17:21:47 +0000

    Style the book store to suite the original design

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
 
 class Header extends React.Component {
   render() {
     return (
 
       <nav className="navigation">
-        <Link to="/" className="logoStyle">Book Store Logo</Link>
+        <Link to="/" className="logoStyle">Bookstore CMS</Link>
 
         <ul className="nav-header">
           <li className="nav-link">
-            <Link className="nav-link" to="/Home">Book</Link>
+            <Link className="nav-link active" to="/Home">Books</Link>
           </li>
           <li className="nav-link">
             <Link className="nav-link" to="/Catogeries">Categories</Link>
           </li>
+          <ImUser className="nav-image" style={{ color: '#0290ff', fontSize: '16px', marginLeft:'400px', borderRadius:'50%', border:'0.5px solid blue', padding:'5px 5px' }} />
           
 
         </ul>
